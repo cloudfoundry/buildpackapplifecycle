241b0b310ac80142758e073d0cd8bf3327b4ce07
Onsi Fakhouri <onsi@pivotallabs.com>
2014-02-26 11:17:16 -0800

    Fleshed out integration tests
    Signed-off-by: Abhijit Hiremagalur <abhi@pivotallabs.com>

--- main_test.go
+++ main_test.go
@@ -14,19 +14,28 @@ import (
 )
 
 var _ = Describe("Smelting", func() {
-	buildpackFixtures := "./fixtures/buildpacks"
-	appFixtures := "./fixtures/apps"
-
-	var smelterCmd *exec.Cmd
+	buildpackFixtures := "fixtures/buildpacks"
+	appFixtures := "fixtures/apps"
 
 	var (
-		appDir         string
-		buildpacksDir  string
-		outputDir      string
-		cacheDir       string
-		buildpackOrder string
-		resultDir      string
+		smelterCmd    *exec.Cmd
+		appDir        string
+		buildpacksDir string
+		outputDir     string
+		cacheDir      string
+		resultDir     string
 	)
+
+	smelt := func() *cmdtest.Session {
+		session, err := cmdtest.StartWrapped(
+			smelterCmd,
+			runner_support.TeeIfVerbose,
+			runner_support.TeeIfVerbose,
+		)
+		Ω(err).ShouldNot(HaveOccurred())
+
+		return session
+	}
 
 	BeforeEach(func() {
 		smelterCmd = exec.Command(smelterPath)
@@ -47,6 +56,21 @@ var _ = Describe("Smelting", func() {
 
 		resultDir, err = ioutil.TempDir(os.TempDir(), "smelting-result")
 		Ω(err).ShouldNot(HaveOccurred())
+
+		smelterCmd.Env = append(
+			os.Environ(),
+			"APP_DIR="+appDir,
+			"BUILDPACKS_DIR="+buildpacksDir,
+			"OUTPUT_DIR="+outputDir,
+			"CACHE_DIR="+cacheDir,
+			"BUILDPACK_ORDER=always-detects",
+			"RESULT_DIR="+resultDir,
+		)
+
+		cp(path.Join(buildpackFixtures, "always-detects"), buildpacksDir)
+		cp(path.Join(appFixtures, "bash-app")+"/", appDir)
+
+		Ω(smelt()).Should(ExitWith(0))
 	})
 
 	AfterEach(func() {
@@ -55,65 +79,53 @@ var _ = Describe("Smelting", func() {
 		os.RemoveAll(outputDir)
 	})
 
-	JustBeforeEach(func() {
-		smelterCmd.Env = append(
-			os.Environ(),
-			"APP_DIR="+appDir,
-			"BUILDPACKS_DIR="+buildpacksDir,
-			"OUTPUT_DIR="+outputDir,
-			"CACHE_DIR="+cacheDir,
-			"BUILDPACK_ORDER="+buildpackOrder,
-			"RESULT_DIR="+resultDir,
-		)
+	It("should produce an /app dir with the contents of the compilation", func() {
+		appDirLocation := path.Join(outputDir, "app")
+		contents, err := ioutil.ReadDir(appDirLocation)
+		Ω(contents, err).Should(HaveLen(2))
+
+		names := []string{contents[0].Name(), contents[1].Name()}
+		Ω(names).Should(ContainElement("app.sh"))
+		Ω(names).Should(ContainElement("compiled"))
 	})
 
-	smelt := func() *cmdtest.Session {
-		session, err := cmdtest.StartWrapped(
-			smelterCmd,
-			runner_support.TeeIfVerbose,
-			runner_support.TeeIfVerbose,
-		)
+	It("should produce a droplet containing an empty /tmp directory", func() {
+		tmpDirLocation := path.Join(outputDir, "tmp")
+		Ω(ioutil.ReadDir(tmpDirLocation)).Should(BeEmpty())
+	})
+
+	It("should produce a droplet containing an empty /logs directory", func() {
+		logsDirLocation := path.Join(outputDir, "logs")
+		Ω(ioutil.ReadDir(logsDirLocation)).Should(BeEmpty())
+	})
+
+	It("should produce a staging_info.yml with the correct information", func() {
+		stagingInfoLocation := path.Join(outputDir, "staging_info.yml")
+		stagingInfo, err := ioutil.ReadFile(stagingInfoLocation)
 		Ω(err).ShouldNot(HaveOccurred())
 
-		return session
-	}
+		expectedYAML := "\"detected_buildpack\": \"Always Matching\"\n\"start_command\": \"the start command\"\n"
+		Ω(string(stagingInfo)).Should(Equal(expectedYAML))
+	})
 
-	cp := func(src string, dst string) {
-		session, err := cmdtest.StartWrapped(
-			exec.Command("cp", "-a", src, dst),
-			runner_support.TeeIfVerbose,
-			runner_support.TeeIfVerbose,
-		)
+	It("produces a result.json", func() {
+		resultLocation := path.Join(resultDir, "result.json")
+		resultInfo, err := ioutil.ReadFile(resultLocation)
 		Ω(err).ShouldNot(HaveOccurred())
-		Ω(session).Should(ExitWith(0))
-	}
+		expectedJSON := `{
+			"detected_buildpack": "Always Matching"
+		}`
 
-	Context("when a buildpack succeeds", func() {
-		BeforeEach(func() {
-			cp(path.Join(buildpackFixtures, "always-detects"), buildpacksDir)
-			cp(path.Join(appFixtures, "bash-app/"), appDir)
-
-			buildpackOrder = "always-detects"
-		})
-
-		It("produces a droplet", func() {
-			Ω(smelt()).Should(ExitWith(0))
-
-			fileInfo, err := os.Stat(path.Join(outputDir, "app"))
-			Ω(err).ShouldNot(HaveOccurred())
-			Ω(fileInfo.IsDir()).Should(BeTrue())
-
-			fileInfo, err = os.Stat(path.Join(outputDir, "tmp"))
-			Ω(err).ShouldNot(HaveOccurred())
-			Ω(fileInfo.IsDir()).Should(BeTrue())
-
-			fileInfo, err = os.Stat(path.Join(outputDir, "logs"))
-			Ω(err).ShouldNot(HaveOccurred())
-			Ω(fileInfo.IsDir()).Should(BeTrue())
-
-			fileInfo, err = os.Stat(path.Join(outputDir, "staging_info.yml"))
-			Ω(err).ShouldNot(HaveOccurred())
-			Ω(fileInfo.IsDir()).Should(BeFalse())
-		})
+		Ω(resultInfo).Should(MatchJSON(expectedJSON))
 	})
 })
+
+func cp(src string, dst string) {
+	session, err := cmdtest.StartWrapped(
+		exec.Command("cp", "-a", src, dst),
+		runner_support.TeeIfVerbose,
+		runner_support.TeeIfVerbose,
+	)
+	Ω(err).ShouldNot(HaveOccurred())
+	Ω(session).Should(ExitWith(0))
+}
