ddc9ed4a5f80d9cfc6598dafc013ae0bedc96b19
Onsi Fakhouri <onsi@pivotallabs.com>
2014-03-19 14:32:53 -0700

    bump deps
    Signed-off-by: Aram Price <aprice@pivotallabs.com>

--- Godeps/_workspace/src/github.com/cloudfoundry-incubator/runtime-schema/models/smelting_configs.go
+++ Godeps/_workspace/src/github.com/cloudfoundry-incubator/runtime-schema/models/smelting_configs.go
@@ -1,68 +1,180 @@
 package models
 
 import (
+	"flag"
+	"fmt"
 	"path"
 	"strings"
 )
 
 type LinuxSmeltingConfig struct {
-	buildpackOrder []string
+	*flag.FlagSet
+
+	compilerPath string
+
+	values map[string]*string
+
+	appDir         *string
+	outputDir      *string
+	resultDir      *string
+	buildpacksDir  *string
+	cacheDir       *string
+	buildpackOrder *string
 }
 
-func NewLinuxSmeltingConfig(buildpackOrder []string) LinuxSmeltingConfig {
+const (
+	LinuxSmeltingAppDirFlag         = "appDir"
+	LinuxSmeltingOutputDirFlag      = "outputDir"
+	LinuxSmeltingResultDirFlag      = "resultDir"
+	LinuxSmeltingBuildpacksDirFlag  = "buildpacksDir"
+	LinuxSmeltingCacheDirFlag       = "cacheDir"
+	LinuxSmeltingBuildpackOrderFlag = "buildpackOrder"
+)
+
+var LinuxSmeltingDefaults = map[string]string{
+	LinuxSmeltingAppDirFlag:        "/app",
+	LinuxSmeltingOutputDirFlag:     "/tmp/droplet",
+	LinuxSmeltingResultDirFlag:     "/tmp/result",
+	LinuxSmeltingBuildpacksDirFlag: "/tmp/buildpacks",
+	LinuxSmeltingCacheDirFlag:      "/tmp/cache",
+}
+
+func NewLinuxSmeltingConfig(buildpacks []string) LinuxSmeltingConfig {
+	flagSet := flag.NewFlagSet("linux-smelter", flag.ExitOnError)
+
+	appDir := flagSet.String(
+		LinuxSmeltingAppDirFlag,
+		LinuxSmeltingDefaults[LinuxSmeltingAppDirFlag],
+		"directory containing raw app bits",
+	)
+
+	outputDir := flagSet.String(
+		LinuxSmeltingOutputDirFlag,
+		LinuxSmeltingDefaults[LinuxSmeltingOutputDirFlag],
+		"directory in which to write the smelted app bits",
+	)
+
+	resultDir := flagSet.String(
+		LinuxSmeltingResultDirFlag,
+		LinuxSmeltingDefaults[LinuxSmeltingResultDirFlag],
+		"directory in which to place smelting result metadata",
+	)
+
+	buildpacksDir := flagSet.String(
+		LinuxSmeltingBuildpacksDirFlag,
+		LinuxSmeltingDefaults[LinuxSmeltingBuildpacksDirFlag],
+		"directory containing the buildpacks to try",
+	)
+
+	cacheDir := flagSet.String(
+		LinuxSmeltingCacheDirFlag,
+		LinuxSmeltingDefaults[LinuxSmeltingCacheDirFlag],
+		"directory to store cached artifacts to buildpacks",
+	)
+
+	buildpackOrder := flagSet.String(
+		LinuxSmeltingBuildpackOrderFlag,
+		strings.Join(buildpacks, ","),
+		"comma-separated list of buildpacks, to be tried in order",
+	)
+
+	compilerPath := "/tmp/compiler"
+
 	return LinuxSmeltingConfig{
+		FlagSet: flagSet,
+
+		compilerPath: compilerPath,
+
+		appDir:         appDir,
+		outputDir:      outputDir,
+		resultDir:      resultDir,
+		buildpacksDir:  buildpacksDir,
+		cacheDir:       cacheDir,
 		buildpackOrder: buildpackOrder,
+
+		values: map[string]*string{
+			"appDir":         appDir,
+			"outputDir":      outputDir,
+			"resultDir":      resultDir,
+			"buildpacksDir":  buildpacksDir,
+			"cacheDir":       cacheDir,
+			"buildpackOrder": buildpackOrder,
+		},
 	}
 }
 
 func (s LinuxSmeltingConfig) Script() string {
-	return strings.Join([]string{
-		s.compilerCommand(),
-		"-appDir", s.AppPath(),
-		"-outputDir", s.dropletDirPath(),
-		"-resultDir", s.resultJsonDir(),
-		"-buildpacksDir", s.buildpacksDir(),
-		"-buildpackOrder", strings.Join(s.buildpackOrder, ","),
-		"-cacheDir", s.cacheDir(),
-	}, " ")
+	argv := []string{s.compilerCommand()}
+
+	s.FlagSet.VisitAll(func(flag *flag.Flag) {
+		argv = append(argv, fmt.Sprintf("-%s='%s'", flag.Name, *s.values[flag.Name]))
+	})
+
+	return strings.Join(argv, " ")
 }
 
-func (s LinuxSmeltingConfig) AppPath() string {
-	return "/app"
+func (s LinuxSmeltingConfig) Validate() error {
+	var missingFlags []string
+
+	s.FlagSet.VisitAll(func(flag *flag.Flag) {
+		schemaFlag, ok := s.values[flag.Name]
+		if !ok {
+			return
+		}
+
+		value := *schemaFlag
+		if value == "" {
+			missingFlags = append(missingFlags, "-"+flag.Name)
+		}
+	})
+
+	if len(missingFlags) > 0 {
+		return fmt.Errorf("missing flags: %s", strings.Join(missingFlags, ", "))
+	}
+
+	return nil
 }
 
-func (s LinuxSmeltingConfig) buildpacksDir() string {
-	return path.Join("/tmp", "buildpacks")
+func (s LinuxSmeltingConfig) AppDir() string {
+	return *s.appDir
 }
 
 func (s LinuxSmeltingConfig) BuildpackPath(buildpackName string) string {
-	return path.Join(s.buildpacksDir(), buildpackName)
+	return path.Join(s.BuildpacksDir(), buildpackName)
+}
+
+func (s LinuxSmeltingConfig) BuildpackOrder() []string {
+	return strings.Split(*s.buildpackOrder, ",")
+}
+
+func (s LinuxSmeltingConfig) BuildpacksDir() string {
+	return *s.buildpacksDir
+}
+
+func (s LinuxSmeltingConfig) CacheDir() string {
+	return *s.cacheDir
+}
+
+func (s LinuxSmeltingConfig) CompilerPath() string {
+	return s.compilerPath
 }
 
 func (s LinuxSmeltingConfig) compilerCommand() string {
 	return path.Join(s.CompilerPath(), "run")
 }
 
-func (s LinuxSmeltingConfig) CompilerPath() string {
-	return path.Join("/tmp", "compiler")
-}
-
-func (s LinuxSmeltingConfig) cacheDir() string {
-	return path.Join("/tmp", "cache")
-}
-
-func (s LinuxSmeltingConfig) dropletDirPath() string {
-	return path.Join("/tmp", "droplet")
-}
-
 func (s LinuxSmeltingConfig) DropletArchivePath() string {
-	return path.Join(s.dropletDirPath(), "droplet.tgz")
+	return path.Join(s.OutputDir(), "droplet.tgz")
 }
 
-func (s LinuxSmeltingConfig) resultJsonDir() string {
-	return path.Join("/tmp", "result")
+func (s LinuxSmeltingConfig) OutputDir() string {
+	return *s.outputDir
+}
+
+func (s LinuxSmeltingConfig) ResultJsonDir() string {
+	return *s.resultDir
 }
 
 func (s LinuxSmeltingConfig) ResultJsonPath() string {
-	return path.Join(s.resultJsonDir(), "result.json")
+	return path.Join(s.ResultJsonDir(), "result.json")
 }
--- Godeps/_workspace/src/github.com/cloudfoundry-incubator/runtime-schema/models/smelting_configs_test.go
+++ Godeps/_workspace/src/github.com/cloudfoundry-incubator/runtime-schema/models/smelting_configs_test.go
@@ -1,6 +1,8 @@
 package models_test
 
 import (
+	"strings"
+
 	. "github.com/cloudfoundry-incubator/runtime-schema/models"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
@@ -8,20 +10,55 @@ import (
 
 var _ = Describe("LinuxSmeltingConfig", func() {
 	var smeltingConfig LinuxSmeltingConfig
+
 	BeforeEach(func() {
 		smeltingConfig = NewLinuxSmeltingConfig([]string{"ocaml-buildpack", "haskell-buildpack", "bash-buildpack"})
 	})
 
-	It("generates a script for running its smelter", func() {
-		expectedScript := "/tmp/compiler/run" +
-			" -appDir /app" +
-			" -outputDir /tmp/droplet" +
-			" -resultDir /tmp/result" +
-			" -buildpacksDir /tmp/buildpacks" +
-			" -buildpackOrder ocaml-buildpack,haskell-buildpack,bash-buildpack" +
-			" -cacheDir /tmp/cache"
+	Context("with defaults", func() {
+		It("generates a script for running its smelter", func() {
+			expectedScript := strings.Join(
+				[]string{
+					"/tmp/compiler/run",
+					"-appDir='/app'",
+					"-buildpackOrder='ocaml-buildpack,haskell-buildpack,bash-buildpack'",
+					"-buildpacksDir='/tmp/buildpacks'",
+					"-cacheDir='/tmp/cache'",
+					"-outputDir='/tmp/droplet'",
+					"-resultDir='/tmp/result'",
+				},
+				" ",
+			)
 
-		Ω(smeltingConfig.Script()).To(Equal(expectedScript))
+			Ω(smeltingConfig.Script()).To(Equal(expectedScript))
+		})
+	})
+
+	Context("with overrides", func() {
+		BeforeEach(func() {
+			smeltingConfig.Set(LinuxSmeltingAppDirFlag, "/some/app/dir")
+			smeltingConfig.Set(LinuxSmeltingOutputDirFlag, "/some/droplet/dir")
+			smeltingConfig.Set(LinuxSmeltingResultDirFlag, "/some/result/dir")
+			smeltingConfig.Set(LinuxSmeltingBuildpacksDirFlag, "/some/buildpacks/dir")
+			smeltingConfig.Set(LinuxSmeltingCacheDirFlag, "/some/cache/dir")
+		})
+
+		It("generates a script for running its smelter", func() {
+			expectedScript := strings.Join(
+				[]string{
+					"/tmp/compiler/run",
+					"-appDir='/some/app/dir'",
+					"-buildpackOrder='ocaml-buildpack,haskell-buildpack,bash-buildpack'",
+					"-buildpacksDir='/some/buildpacks/dir'",
+					"-cacheDir='/some/cache/dir'",
+					"-outputDir='/some/droplet/dir'",
+					"-resultDir='/some/result/dir'",
+				},
+				" ",
+			)
+
+			Ω(smeltingConfig.Script()).To(Equal(expectedScript))
+		})
 	})
 
 	It("returns the path to the compiler", func() {
@@ -29,7 +66,7 @@ var _ = Describe("LinuxSmeltingConfig", func() {
 	})
 
 	It("returns the path to the app bits", func() {
-		Ω(smeltingConfig.AppPath()).To(Equal("/app"))
+		Ω(smeltingConfig.AppDir()).To(Equal("/app"))
 	})
 
 	It("returns the path to a given buildpack", func() {
