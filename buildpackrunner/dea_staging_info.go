fed2a815970e3d968a8e112d3cbef255c51e909e
Nick Wade <nwade@pivotal.io>
2014-10-09 14:11:16 -0700

    go fmt [#79566468]
    Signed-off-by: Abhijit Hiremagalur <abhi@pivotal.io>

--- buildpackrunner/dea_staging_info.go
+++ buildpackrunner/dea_staging_info.go
@@ -3,6 +3,5 @@ package buildpackrunner
 // Used to generate YAML file read by the DEA
 type DeaStagingInfo struct {
 	DetectedBuildpack string `yaml:"detected_buildpack"`
-	StartCommand string `yaml:"start_command"`
+	StartCommand      string `yaml:"start_command"`
 }
-
