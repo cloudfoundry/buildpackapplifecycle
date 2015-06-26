855f14e280c0675fc752b6c769437585e01f11d3
JT Archie <jtarchie@gmail.com>
2015-06-26 10:48:44 -0400

    ensure submodules are updates on git branches [#97854548]
    Signed-off-by: Anthony Emengo <aemengo@pivotal.io>

--- buildpackrunner/buildpackrunner_suite_test.go
+++ buildpackrunner/buildpackrunner_suite_test.go
@@ -34,22 +34,38 @@ var _ = SynchronizedBeforeSuite(func() []byte {
 	err = os.MkdirAll(buildpackDir, os.ModePerm)
 	Expect(err).NotTo(HaveOccurred())
 
+	submoduleDir := filepath.Join(tmpDir, "submodule")
+	err = os.MkdirAll(submoduleDir, os.ModePerm)
+	Expect(err).NotTo(HaveOccurred())
+
 	execute(buildpackDir, "rm", "-rf", ".git")
 	execute(buildpackDir, gitPath, "init")
 	execute(buildpackDir, gitPath, "config", "user.email", "you@example.com")
 	execute(buildpackDir, gitPath, "config", "user.name", "your name")
+	writeFile(filepath.Join(buildpackDir, "content"), "some content")
 
-	err = ioutil.WriteFile(filepath.Join(buildpackDir, "content"),
-		[]byte("some content"), os.ModePerm)
-	Expect(err).NotTo(HaveOccurred())
+	execute(submoduleDir, "rm", "-rf", ".git")
+	execute(submoduleDir, gitPath, "init")
+	execute(submoduleDir, gitPath, "config", "user.email", "you@example.com")
+	execute(submoduleDir, gitPath, "config", "user.name", "your name")
+	writeFile(filepath.Join(submoduleDir, "README"), "1st commit")
+	execute(submoduleDir, gitPath, "add", ".")
+	execute(submoduleDir, gitPath, "commit", "-am", "first commit")
+	writeFile(filepath.Join(submoduleDir, "README"), "2nd commit")
+	execute(submoduleDir, gitPath, "commit", "-am", "second commit")
 
-	execute(buildpackDir, gitPath, "add", ".")
+	execute(buildpackDir, gitPath, "submodule", "add", "file://"+submoduleDir, "sub")
+	execute(buildpackDir+"/sub", gitPath, "checkout", "HEAD^")
 	execute(buildpackDir, gitPath, "add", "-A")
-	execute(buildpackDir, gitPath, "commit", "-am", "fake commit")
+	execute(buildpackDir, gitPath, "commit", "-m", "fake commit")
 	execute(buildpackDir, gitPath, "commit", "--allow-empty", "-m", "empty commit")
-	execute(buildpackDir, gitPath, "branch", "a_branch")
 	execute(buildpackDir, gitPath, "tag", "-m", "annotated tag", "a_tag")
 	execute(buildpackDir, gitPath, "tag", "a_lightweight_tag")
+	execute(buildpackDir, gitPath, "checkout", "-b", "a_branch")
+	execute(buildpackDir+"/sub", gitPath, "checkout", "master")
+	execute(buildpackDir, gitPath, "add", "-A")
+	execute(buildpackDir, gitPath, "commit", "-am", "update submodule")
+	execute(buildpackDir, gitPath, "checkout", "master")
 	execute(buildpackDir, gitPath, "update-server-info")
 
 	httpServer = httptest.NewServer(http.FileServer(http.Dir(tmpDir)))
@@ -81,5 +97,11 @@ func execute(dir string, execCmd string, args ...string) {
 	cmd := exec.Command(execCmd, args...)
 	cmd.Dir = dir
 	err := cmd.Run()
+	Expect(err).NotTo(HaveOccurred())
+}
+
+func writeFile(filepath, content string) {
+	err := ioutil.WriteFile(filepath,
+		[]byte(content), os.ModePerm)
 	Expect(err).NotTo(HaveOccurred())
 }
